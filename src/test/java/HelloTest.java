82ad357b6e635cd75d85a96a8dceee18e9a22591
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-02-12 00:10:16 +0100

    movie2 branch added some code

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,5 +2,6 @@ public class HelloTest {
     public static void main(String[] args) {
         System.out.println("Hello world Test");
         System.out.println(" Movie 2 is created");
+        // we are watching movie on phone
     }
 }
