4419328b32d9b5c3a68fab07517e79fa074fb908
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-02-11 22:56:39 +0100

    Added more to FeatureBTest class

--- src/test/java/FeatureBTest.java
+++ src/test/java/FeatureBTest.java
@@ -1,5 +1,6 @@
 public class FeatureBTest {
     public static void main(String[] args) {
         System.out.println("Feature B is created");
+        System.out.println("More stuff here");
     }
 }
