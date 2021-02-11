50dbf80f86534c427c274fbad6ae907767dc80d0
AbdusadikSattar <71940275+AbdusadikSattar@users.noreply.github.com>
2021-02-11 23:01:33 +0100

    Added more to FeatureCClass class

--- src/test/java/FeatureCClass.java
+++ src/test/java/FeatureCClass.java
@@ -2,5 +2,6 @@ public class FeatureCClass {
     public static void main(String[] args) {
         System.out.println("Created Feature C class");
 
+        System.out.println(" more code here");
     }
 }
